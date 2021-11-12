7c798c3a8b1df8f687b559e73cd3878602206abe
Moemen <s1127426@student.hsleiden.nl>
2021-11-12 02:46:14 +0100

    dashboard kinda done

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -1,6 +1,41 @@
 <template>
     <div>
-        <p>dit is het dashboard</p>
+        <v-row>
+            <v-col>
+                <v-card class="card-container">
+                    <div>
+                        Total users:
+                    </div>
+                    <p >
+                        {{users}}
+                    </p>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card class="card-container">
+                    <div>Total Contracts:</div>
+                    <p>{{contracts}}</p>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card class="card-container">
+                    <div>
+                        Total Tickets:
+                    </div>
+                    <p>{{tickets}}</p>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card class="card-container">
+                    <div>
+                        Total companies:
+                    </div>
+                    <p>
+                        {{companies}}
+                    </p>
+                </v-card>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
@@ -11,8 +46,14 @@ export default {
     },
     data() {
         return {
-            
+            users:[],
+            contracts:[],
+            tickets:[],
+            companies:[],
         }
+    },
+    created() {
+        this.getEverything()
     },
     computed: {
 
@@ -24,7 +65,41 @@ export default {
 
     },
     methods: {
-        
+        getEverything(){
+            this.getAllUsers()
+            this.getAllContracts()
+            this.getAllTickets()
+            this.getAllCompanies()
+        },
+        getAllUsers(){
+            this.$axios
+                .get('api/users')
+                .then((response) => {
+                    this.users = response.data.length
+                })
+                
+        },
+        getAllContracts(){
+            this.$axios
+                .get('api/contract')
+                .then((response) => {
+                    this.contracts = response.data.length
+                })
+        },
+        getAllTickets(){
+            this.$axios
+                .get('api/ticket')
+                .then((response) => {
+                    this.tickets = response.data.length
+                })
+        },
+        getAllCompanies(){
+            this.$axios
+                .get('api/company')
+                .then((response) => {
+                    this.companies = response.data.length
+                })
+        }
     }
 }
 </script>
